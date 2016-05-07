6019b6891614c8da8cf90ce1b12730d1132a2f90
Etienne Whittom <etienne.whittom@gmail.com>
2016-05-07 10:33:38 -0400

    Adding Zombie model from the asset store, and Pooling test scene.

---
+++ Assets/Scripts/PoolingTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using RainyDays;
+
+public class PoolingTest : MonoBehaviour {
+
+	public float waitSeconds = 1.0f;
+	public Pool pool;
+	public int acquireCount = 1;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		
+		yield return new WaitForSeconds(waitSeconds);
+		for (int i = 0; i < acquireCount; ++i) {
+			pool.Ask (this.transform, (r) => {
+				r.result.transform.position = new Vector3 ((float)r.result.IndexInPool, 0.0f, 0.0f);
+			});
+		}
+	}
+}
